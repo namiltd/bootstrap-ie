7ea4e7ea572709cfbc7d7da842a640d612772ed6
namiltd <namiltd@users.noreply.github.com>
2018-08-28 23:00:46 +0200

    code cleaning

--- js/bootstrap-ie9.js
+++ js/bootstrap-ie9.js
@@ -34,17 +34,17 @@
             setClass();
         };
         domTokenList.add=function(){
-            var args=[].slice.call(arguments)
+            var args=[].slice.call(arguments);
             for (var i=0,l=args.length;i<l;i++){
-                if (domTokenList.indexOf(args[i])===-1) domTokenList.push(args[i])
-            };
+                if (domTokenList.indexOf(args[i])===-1) domTokenList.push(args[i]);
+            }
             setClass();
         };
         domTokenList.remove=function(){
-            var args=[].slice.call(arguments)
+            var args=[].slice.call(arguments);
             for (var i=0,l=args.length;i<l;i++){
                 if (domTokenList.indexOf(args[i])!==-1) domTokenList.splice(domTokenList.indexOf(args[i]),1);
-            };
+            }
             setClass();
         };
         domTokenList.item=function(i){
@@ -60,7 +60,7 @@
         domTokenList.value = (element.getAttribute('class')||'');
         return domTokenList;
     }
-  })
+  });
 })();
 
 /**
